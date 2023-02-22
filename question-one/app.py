9f4c8454a56317fc71edd9eda3566d99ddf17bba
Joao <jaotoledo74@gmail.com>
2023-02-22 16:18:31 -0400

    Renaming variables

--- question-one/app.py
+++ question-one/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-bills_df = pd.read_csv('bills.csv')
-legislators_df = pd.read_csv('legislators.csv')
-vote_results_df = pd.read_csv('vote_results.csv')
-votes_df = pd.read_csv('votes.csv')
+bills = pd.read_csv('bills.csv')
+legislators = pd.read_csv('legislators.csv')
+vote_results = pd.read_csv('vote_results.csv')
+votes = pd.read_csv('votes.csv')
+
