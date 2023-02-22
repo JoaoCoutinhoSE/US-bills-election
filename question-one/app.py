94c1fe0f04ecc20456e239b6d38210d6f0f43168
Joao <jaotoledo74@gmail.com>
2023-02-22 19:17:01 -0400

    Pandas as import instead of pd

--- question-one/app.py
+++ question-one/app.py
@@ -1,7 +1,7 @@
-import pandas as pd
+import pandas as pandas
 
-bills = pd.read_csv('bills.csv')
-legislators = pd.read_csv('legislators.csv')
-vote_results = pd.read_csv('vote_results.csv')
-votes = pd.read_csv('votes.csv')
+bills = pandas.read_csv('bills.csv')
+legislators = pandas.read_csv('legislators.csv')
+vote_results = pandas.read_csv('vote_results.csv')
+votes = pandas.read_csv('votes.csv')
 
