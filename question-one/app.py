e950476d757b07700fcc5036dbbd3bf0b01c2b73
Joao <jaotoledo74@gmail.com>
2023-02-22 15:50:22 -0400

    Loading data-sets at main file

---
+++ question-one/app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+bills_df = pd.read_csv('bills.csv')
+legislators_df = pd.read_csv('legislators.csv')
+vote_results_df = pd.read_csv('vote_results.csv')
+votes_df = pd.read_csv('votes.csv')
