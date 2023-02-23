ed1128423164706c4fa94d26678e8ccea99e12d0
Joao <jaotoledo74@gmail.com>
2023-02-23 15:19:32 -0400

    Printing result of merge

--- question-one/app.py
+++ question-one/app.py
@@ -1,7 +1,10 @@
-import pandas as pandas
+import pandas
 
-bills = pandas.read_csv('bills.csv')
-legislators = pandas.read_csv('legislators.csv')
-vote_results = pandas.read_csv('vote_results.csv')
-votes = pandas.read_csv('votes.csv')
+bills_df = pandas.read_csv('../datasets/bills.csv')
+legislators_df = pandas.read_csv('../datasets/legislators.csv')
+vote_results_df = pandas.read_csv('../datasets/vote_results.csv')
+votes_df = pandas.read_csv('../datasets/votes.csv')
 
+vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df, on='id', how='left')
+
+print(vote_results_with_legislators_df)
