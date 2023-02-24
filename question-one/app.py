2a9333bd45f44a8ffdcde886e58aabb37bd85bc5
Joao <jaotoledo74@gmail.com>
2023-02-24 17:51:05 -0400

    Creating a new column by indexing legislator_id and the name

--- question-one/app.py
+++ question-one/app.py
@@ -13,3 +13,6 @@ vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df,
 
 legislator_counts_df = vote_results_with_legislators_df.groupby(['legislator_id','name', 'vote_type'])['vote_type']\
     .count().reset_index(name='vote_count')
+legislator_counts_df = legislator_counts_df.pivot_table(index=['legislator_id', 'name'], columns='vote_type',
+                                                        values='vote_count', aggfunc=numpy.sum).reset_index()
+
