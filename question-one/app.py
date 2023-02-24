0bd4b39b9481f0160c988c33775dae2d076af29a
Joao <jaotoledo74@gmail.com>
2023-02-24 17:50:18 -0400

    Grouping by 'vote_type

--- question-one/app.py
+++ question-one/app.py
@@ -1,4 +1,6 @@
 import pandas
+import numpy
+pandas.set_option('display.expand_frame_repr', False)
 
 bills_df = pandas.read_csv('../datasets/bills.csv')
 legislators_df = pandas.read_csv('../datasets/legislators.csv')
@@ -8,3 +10,6 @@ votes_df = pandas.read_csv('../datasets/votes.csv')
 vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df,  left_on='legislator_id',
                                                 right_on='id',
                                                 how='left')
+
+legislator_counts_df = vote_results_with_legislators_df.groupby(['legislator_id','name', 'vote_type'])['vote_type']\
+    .count().reset_index(name='vote_count')
