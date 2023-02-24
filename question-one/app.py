67632aa840be9ceec23368e1efade08f9cb5781a
Joao <jaotoledo74@gmail.com>
2023-02-24 15:03:02 -0400

    Get results form merging vote results with legislators

--- question-one/app.py
+++ question-one/app.py
@@ -5,6 +5,4 @@ legislators_df = pandas.read_csv('../datasets/legislators.csv')
 vote_results_df = pandas.read_csv('../datasets/vote_results.csv')
 votes_df = pandas.read_csv('../datasets/votes.csv')
 
-vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df, on='id', how='left')
-
-print(vote_results_with_legislators_df)
+vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df,  left_on='legislator_id', right_on='id', how='right')
