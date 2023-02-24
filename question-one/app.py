c73ae4c27f208bc8655173d138b418cb67ee6577
Joao <jaotoledo74@gmail.com>
2023-02-24 15:49:07 -0400

    Fixing bug where at the end of the line were showing 'NaN'

--- question-one/app.py
+++ question-one/app.py
@@ -5,4 +5,6 @@ legislators_df = pandas.read_csv('../datasets/legislators.csv')
 vote_results_df = pandas.read_csv('../datasets/vote_results.csv')
 votes_df = pandas.read_csv('../datasets/votes.csv')
 
-vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df,  left_on='legislator_id', right_on='id', how='right')
+vote_results_with_legislators_df = pandas.merge(vote_results_df, legislators_df,  left_on='legislator_id',
+                                                right_on='id',
+                                                how='left')
