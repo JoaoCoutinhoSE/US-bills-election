fb3e307e391b07e00393e30434e4d0413761a0cf
Joao <jaotoledo74@gmail.com>
2023-02-28 21:25:53 -0400

    Putting df to follow python convention

--- question-two/app.py
+++ question-two/app.py
@@ -6,20 +6,20 @@ legislators_df = pandas.read_csv('../datasets/legislators.csv')
 vote_results_df = pandas.read_csv('../datasets/vote_results.csv')
 votes_df = pandas.read_csv('../datasets/votes.csv')
 
-bills_with_legislators = pandas.merge(bills_df, legislators_df,  left_on='sponsor_id',
+bills_with_legislators_df = pandas.merge(bills_df, legislators_df,  left_on='sponsor_id',
                                                 right_on='id',
                                                 how='left')
 
-bills_with_legislators = bills_with_legislators.rename(columns={'name' : 'primary_sponsor'})
-bills_with_legislators = bills_with_legislators.fillna('Unknown')
+bills_with_legislators_df = bills_with_legislators_df.rename(columns={'name' : 'primary_sponsor'})
+bills_with_legislators_df = bills_with_legislators_df.fillna('Unknown')
 
-bills_with_legislators_and_votes = pandas.merge(bills_with_legislators, votes_df, left_on='id_x', right_on='bill_id',
+bills_with_legislators_and_votes_df = pandas.merge(bills_with_legislators_df, votes_df, left_on='id_x', right_on='bill_id',
                                                 how='left')
-bills_with_legislators_and_votes = bills_with_legislators_and_votes.rename(columns={'id_x' : 'id_billcsv',
+bills_with_legislators_and_votes_df = bills_with_legislators_and_votes_df.rename(columns={'id_x' : 'id_billcsv',
                                                                                     'id_y' : 'id_legislatorcsv',
                                                                                     'id' : 'id_votescsv'})
 
-bills_with_legislators_and_votes_with_votes_results = pandas.merge(bills_with_legislators_and_votes, vote_results_df,
+bills_with_legislators_and_votes_with_votes_results_df = pandas.merge(bills_with_legislators_and_votes_df, vote_results_df,
                                                                    left_on='id_votescsv',
                                                                    right_on='vote_id', how='left')
-print(bills_with_legislators_and_votes_with_votes_results.to_string())
+print(bills_with_legislators_and_votes_with_votes_results_df.to_string())
